316443c3fa57a795dd719dd13335d8fe6d154212
Mary Awopileda <mary.a.awopileda@vanderbilt.edu>
2023-10-15 20:06:41 -0500

    changes

---
+++ server/controllers/userController.js
@@ -0,0 +1,43 @@
+const firebase = require("firebase/database");
+
+
+const test = async (req, res) => {
+    const { testdata } = req.body;
+  
+    // update the user's instruments
+    try {
+      const db = firebase.getDatabase();
+      await firebase.set(firebase.ref(db, `users/`), testdata)
+      return res.status(200).json(); // 200 is an error code for success
+    } catch (error) {
+      return res.status(400).json({ error: error.message })
+    }
+}
+//Hello 
+
+
+const getUser = async (req, res) => {
+//   const uid = req.params.uid;
+
+//   // fetch from the database
+//   try {
+//     const dbRef = firebase.ref(firebase.getDatabase());
+//     firebase.get(firebase.child(dbRef, `users/${uid}`)).then((snapshot) => {
+//       if (snapshot.exists()) {
+//         return res.status(200).json(snapshot.val());
+//       } else {
+//         console.log("No data available");
+//       }
+//     }).catch((error) => {
+//       console.error(error);
+//     });
+//   } catch (error) {
+//     res.status(400).json({ error: error.message })
+//   }
+}
+
+
+module.exports = {
+  getUser,
+  test
+};
--- server/server.js
+++ server/server.js
@@ -1,11 +1,40 @@
+const firebase = require("firebase/app");
 const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 5001;
 
-app.get('/', (req, res) => {
-    res.send('Hello from the backend!');
-});
+const userRoutes = require('./routes/userRoutes')
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+const PORT = 4000
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCc7X1IWwzV-n2eIDcSe605brdRWAHYQrQ",
+    authDomain: "swe-project-304dd.firebaseapp.com",
+    databaseURL: "https://swe-project-304dd-default-rtdb.firebaseio.com",
+    projectId: "swe-project-304dd",
+    storageBucket: "swe-project-304dd.appspot.com",
+    messagingSenderId: "414472923061",
+    appId: "1:414472923061:web:f9cc87465d661fee24dbfc",
+    measurementId: "G-7TWZD35N23"
+  };
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// create express app
+const app = express()
+
+// checks if request has body data (e.g. post, patch)
+// if it does, passes that data to req object
+app.use(express.json())
+
+// will run every time a request comes in, simply logs for debug
+app.use((req, res, next) => {
+  console.log('connected to path:', req.path, 'with method:', req.method)
+  next()
+})
+
+// if we get a request from that page
+app.use('/api/user', userRoutes);
+
+const server = app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
