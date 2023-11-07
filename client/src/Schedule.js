4c04f9cb27df78c3148c4595d18b293463860982
reesetateo <reese.l.tateo@vanderbilt.edu>
2023-11-06 19:39:47 -0600

    scheduling

--- client/src/Schedule.js
+++ client/src/Schedule.js
@@ -22,12 +22,14 @@ function Schedule() {
     "Alyssa Bersamin",
     "Bita Tavafoghi"
     ];
-    const times = ["8-9am", "9-10am", "10-11am", "11-12pm"];
-    
+    const times = ["8:00 am - 9:00 am", "9:00 am - 10:00 am", "10:00 am - 11:00 am", "11:00 am - 12:00 pm", "12:00 pm - 1:00 pm", "1:00 pm - 2:00 pm","2:00 pm - 3:00 pm","3:00 pm - 4:00 pm","5:00 pm - 6:00 pm","6:00 pm - 7:00 pm", "7:00 pm - 8:00 pm"];
+    const dayOfWeek = ["Monday","Tuesday","Wenesday","Thursday","Friday"];
+
 // State to handle search and filter values
 const [searchTerm, setSearchTerm] = useState('');
 const [selectedTrainer, setSelectedTrainer] = useState('');
 const [selectedTime, setSelectedTime] = useState('');
+const [selectedDayOfWeek, setSelectedDayOfWeek] = useState('');
 
 const [currentTrainerName, setCurrentTrainerName] = useState("");
 const [currentTrainerHomeTown, setCurrentTrainerHomeTown] = useState("");
@@ -79,6 +81,47 @@ const handleClick = async () => {
     });
 
 }
+
+const handleClick2 = async () => {
+  try {
+    // Check if a trainer is selected
+    if (selectedTrainer) {
+      // Use the selectedTrainer value in the API call
+      const response = await fetch("http://localhost:4000/api/user/trainer/" + selectedTrainer, {
+        method: "GET",
+      });
+
+      const data = await response.json();
+
+      setCurrentTrainerName(data.name);
+      setCurrentTrainerPosition(data.Position);
+      setCurrentTrainerHomeTown(data.Hometown);
+      setCurrentTrainerTimes(data.Times);
+
+      // When we fetch the data, we also need to set the length of the selectedDayTime array to 10 falses or however many date time combos you have
+      let arr = [];
+      for (let i = 0; i < days.length; i++) {
+        arr.push(false);
+      }
+      setSelectedDayTime(arr);
+
+      if (data.Times) {
+        setDays(Object.keys(data.Times));
+        setTime(Object.values(data.Times));
+      }
+
+      // Add code to show the trainer's information in a popup or modal
+      // You can use a library like Material-UI Dialog for this purpose
+    } else {
+      // Handle the case where no trainer is selected
+      console.warn("Please select a trainer before clicking submit.");
+    }
+
+  } catch (error) {
+    console.error("Error in async", error);
+  }
+};
+
 
 // Have a function which is caleld when the submit button is pressed
 // This will take the boolean array, it will go through each day/time combo
@@ -155,7 +198,7 @@ return (
         <option value=''>Filter by Trainer</option>
         {trainers.map(trainer => <option key={trainer} value={trainer}>{trainer}</option>)}
       </select>
-      <button onClick={handleClick}>Submit</button>
+      <button onClick={handleClick2}>Submit</button>
       
       
       {/* Time filter dropdown */}
@@ -163,7 +206,14 @@ return (
         <option value=''>Filter by Time</option>
         {times.map(time => <option key={time} value={time}>{time}</option>)}
       </select>
-      
+      <button onClick={handleClick2}>Submit</button>
+
+      {/* Day of Week filter dropdown */}
+      <select value={selectedTime} onChange={(e) => setSelectedDayOfWeek(e.target.value)}>
+        <option value=''>Filter by Day</option>
+        {dayOfWeek.map(dayOfWeek => <option key={dayOfWeek} value={dayOfWeek}>{dayOfWeek}</option>)}
+      </select>
+      <button onClick={handleClick2}>Submit</button>
       
       {/* Placeholder for schedule content */}
       <p>*Insert schedule feature here*</p>
@@ -172,7 +222,7 @@ return (
           <h3>Trainer: {currentTrainerName}</h3>
           <h3>Hometown: {currentTrainerHomeTown}</h3>
           <h3>Position: {currentTrainerPosition}</h3>
-          <h3>Times:</h3> {displayTimes()}
+          <h3>Availability:</h3> {displayTimes()}
           <button onClick={handleCheckSubmit(selectedDayTime)}>Submit</button>
         </div>
       }
