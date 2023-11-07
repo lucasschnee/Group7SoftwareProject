6e2fffc77db93aee611fbf6d00a885f026c06a7c
reesetateo <reese.l.tateo@vanderbilt.edu>
2023-11-06 18:54:23 -0600

    schedule updates

--- client/src/Schedule.js
+++ client/src/Schedule.js
@@ -1,5 +1,15 @@
 import React, { useState } from 'react';
 import Footer from './Footer';
+import Checkbox from '@mui/material/Checkbox';
+import FormControlLabel from '@mui/material/FormControlLabel';
+//import BigCalendar from 'react-big-calendar'
+//import "react-big-calendar/lib/css/react-big-calendar.css";
+
+//moment.locale("en-GB");
+//BigCalendar.momentLocalizer(moment);
+
+
+
 
 function Schedule() {
     // Example list of trainers and times for filtering
@@ -14,110 +24,165 @@ function Schedule() {
     ];
     const times = ["8-9am", "9-10am", "10-11am", "11-12pm"];
     
-    
-    // State to handle search and filter values
-    const [searchTerm, setSearchTerm] = useState('');
-    const [selectedTrainer, setSelectedTrainer] = useState('');
-    const [selectedTime, setSelectedTime] = useState('');
+// State to handle search and filter values
+const [searchTerm, setSearchTerm] = useState('');
+const [selectedTrainer, setSelectedTrainer] = useState('');
+const [selectedTime, setSelectedTime] = useState('');
 
-    const handleClick = async () => {
-        console.log("working");
+const [currentTrainerName, setCurrentTrainerName] = useState("");
+const [currentTrainerHomeTown, setCurrentTrainerHomeTown] = useState("");
+const [currentTrainerPosition, setCurrentTrainerPosition] = useState("");
+const [currentTrainerTimes, setCurrentTrainerTimes] = useState("");
 
-        fetch("http://localhost:4000/api/user/trainer/"+searchTerm, {
-        method: "GET",
-      })
-        .then(function(response) {
-          // This right here is the response we will receive before we convert
-          // it over to readable JSON
-          console.log("Response turning to readable JSON", response);
-          return response.json();
-        })
-        .then(function(response) {
-          // Don't worry if this says "false", it just returns that when the user already exists from flasks end
-          // Once it reaches here, the async is already successful
-          console.log("Successful async: ", JSON.stringify(response));
-          console.log("realdata",response);
-          console.log(response.Hometown);
-          console.log(response.Position);
-          console.log(response.Name);
-
-         
-            let li = document.createElement("li");
-            let li2 = document.createElement("li");
-            let li3 = document.createElement("li");
-            let name = document.createTextNode(JSON.stringify(response.name));
-            let hometown = document.createTextNode(JSON.stringify(response.Hometown));
-            let position = document.createTextNode(JSON.stringify(response.Position));
-
-            li.appendChild(name);
-            li2.appendChild(hometown);
-            li3.appendChild(position);
-
-            // Append LI to UL
-            document.querySelector("#TrainerName").appendChild(li);
-            document.querySelector("#TrainerHometown").appendChild(li2);
-            document.querySelector("#TrainerPosition").appendChild(li3);
-            
+const [days, setDays] = useState([]);
+const [time, setTime] = useState([]);
+// const [checkboxes, setCheckboxes] = useState([]);
+const [selectedDayTime, setSelectedDayTime] = useState([]); // Array of bools, where true means they selected that day/time
+const [checked, setChecked] = useState(false);
 
 
-        })
-        .catch(function(error) {
-          console.log("Error in async", error);
-        });
+const handleClick = async () => {
+    console.log("working");
 
-        // first get potential friend's UID
-        // let response = await fetch('http://localhost:4000/api/user/trainer/'+searchTerm, {
-        //     method: 'GET'
-        // })
-        
-        // if (!response.ok) {
-        //     console.log("no trainer with name")
-        //     return;
-        // } else {
-        //     data = await response.json();
-        //     console.log("found trainer");
-        //     console.log(data);
-        // }
+    fetch("http://localhost:4000/api/user/trainer/"+searchTerm, {
+    method: "GET",
+  })
+    .then(function(response) {
+      // This right here is the response we will receive before we convert
+      // it over to readable JSON
+      console.log("Response turning to readable JSON", response);
+      return response.json();
+    })
+    .then(function(response) {
+      console.log(JSON.stringify(response.name))
+      setCurrentTrainerName(response.name)
+      setCurrentTrainerPosition(response.Position)
+      setCurrentTrainerHomeTown(response.Hometown)
+      setCurrentTrainerTimes(response.Times)
+
+      // When we fetch the data, we also need to set the length of the selectedDayTime array to 10 falses or however many date time combos you have
+      let arr = []
+      for (let i = 0; i < days.length; i++) {
+        arr.push(false)
+      }
+      setSelectedDayTime(arr)
+
+      if (response.Times)
+      {
+        setDays(Object.keys(response.Times))
+        setTime(Object.values(response.Times))
+      }
+
+    })
+    .catch(function(error) {
+      console.log("Error in async", error);
+    });
+
+}
+
+// Have a function which is caleld when the submit button is pressed
+// This will take the boolean array, it will go through each day/time combo
+// If the position in the boolean array is true, then you know that time was selected
+// You can pass it to the backend in a http post
+// And then mark it as taken/remove it
+
+function handleCheckSubmit(boolArray) // this int is the index of the checkbox
+{
+  for (let i = 0; i < boolArray.length; i++) {
+    if(i){
+      console.log(`hello`);
+      // THIS IS WHERE I WANT TO REMOVE SOMETHING FROM THE DATABASE
+
     }
+  }
+}
 
-    
-        
-    return (
-    <div>
+// Accepts index which will be used to set true on selectedDayTime
+// handle change when button pressed accepts an int
+// 
+
+function handleCheck(i) // this int is the index of the checkbox
+{
+  // let arr = selectedDayTime
+  // arr[i] = true;
+  // setSelectedDayTime(arr)
+  const updatedSelectedDayTime = [...selectedDayTime];
+  updatedSelectedDayTime[i] = true;
+  setSelectedDayTime(updatedSelectedDayTime);
+
+}
+
+function displayTimes() {
+  let lines = []
+
+  for (let i = 0; i < days.length; i++){
+    lines.push(
+      <div>
+        <b>{days[i]}</b> : {time[i]}
+        <Checkbox
+          // checked={checked}
+          onChange={() => handleCheck(i)}
+          inputProps={{ 'aria-label': 'controlled' }}
+          color="success"
+          style={{ color: 'white' }}
+        />
+
+      </div>
+    )
+  }
+
+  return lines
+}
+
+
+ 
+return (
+  <div>
     <div className = "schedule">
-    <h2>Schedule a Session</h2>
-    {/* Search bar */}
-    <input
-    type="text"
-    placeholder="Search..."
-    value={searchTerm}
-    onChange={(e) => setSearchTerm(e.target.value)}
-    />
-    <button onClick={handleClick}>Submit</button>
-    
+      <h2>Schedule a Session</h2>
+      {/* Search bar */}
+      <input
+        type="text"
+        placeholder="Search..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      <button onClick={handleClick}>Submit</button>
+      
 
-    {/* Trainer filter dropdown */}
-    <select value={selectedTrainer} onChange={(e) => setSelectedTrainer(e.target.value)}>
-    <option value=''>Filter by Trainer</option>
-    {trainers.map(trainer => <option key={trainer} value={trainer}>{trainer}</option>)}
-    </select>
-    
-    
-    {/* Time filter dropdown */}
-    <select value={selectedTime} onChange={(e) => setSelectedTime(e.target.value)}>
-    <option value=''>Filter by Time</option>
-    {times.map(time => <option key={time} value={time}>{time}</option>)}
-    </select>
-    
-    
-    {/* Placeholder for schedule content */}
-    <p>*Insert schedule feature here*</p>
+      {/* Trainer filter dropdown */}
+      <select value={selectedTrainer} onChange={(e) => setSelectedTrainer(e.target.value)}>
+        <option value=''>Filter by Trainer</option>
+        {trainers.map(trainer => <option key={trainer} value={trainer}>{trainer}</option>)}
+      </select>
+      <button onClick={handleClick}>Submit</button>
+      
+      
+      {/* Time filter dropdown */}
+      <select value={selectedTime} onChange={(e) => setSelectedTime(e.target.value)}>
+        <option value=''>Filter by Time</option>
+        {times.map(time => <option key={time} value={time}>{time}</option>)}
+      </select>
+      
+      
+      {/* Placeholder for schedule content */}
+      <p>*Insert schedule feature here*</p>
+      {currentTrainerName != "" && 
+        <div style={{ textAlign: 'left' }}>
+          <h3>Trainer: {currentTrainerName}</h3>
+          <h3>Hometown: {currentTrainerHomeTown}</h3>
+          <h3>Position: {currentTrainerPosition}</h3>
+          <h3>Times:</h3> {displayTimes()}
+          <button onClick={handleCheckSubmit(selectedDayTime)}>Submit</button>
+        </div>
+      }
+      <h2>Test</h2>
+
     </div>
-    <Footer />
-    </div>
-    );
-    }
-    
-    
-    export default Schedule;
-    
+  <Footer />
+</div>
+);
+}
+
+
+export default Schedule;   
