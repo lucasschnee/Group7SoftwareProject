fd0020671179695264245351aea637bd70ce0502
Lucas Schnee <lucas.j.schnee@vanderbilt.edu>
2023-12-02 09:48:24 -0600

    trainers page updates

--- client/src/Trainers.js
+++ client/src/Trainers.js
@@ -11,56 +11,58 @@ function Trainers() {
     <div className="trainer">
     <h3>Melanie Leguizamon</h3>
     <p>Position: President</p>
-    <p>Hometown:</p>
-    <p>Fun Fact:</p>
+    <p>Hometown: Greenwich, CT</p>
+    <p>Quote: "I have been lifting/bodybuilding for over 2 years, and I currently work two jobs as a certified personal trainer."
+    </p>
     </div>
     
     
     <div className="trainer">
     <h3>Henry Jonokuchi</h3>
     <p>Position: Vice President</p>
-    <p>Hometown:</p>
-    <p>Fun Fact:</p>
+    <p>Hometown: NYC</p>
+    <p>Quote: "’ve been lifting regularly for the past 3 years 
+    after almost 15 years of competitive baseball. When I first stopped baseball, I struggled finding outlets to stay regularly active and fit—this led to a number of injuries during recreational sports, along with overall mental struggles. It wasn’t until I started lifting regularly that I realized just how significant frequent intense exercise was in improving my quality of life. After that, I decided to really start prioritizing my health and longevity."</p>
     </div>
     
     
     <div className="trainer">
     <h3>Matthew Stein II</h3>
     <p>Position: Treasurer</p>
-    <p>Hometown:</p>
-    <p>Fun Fact:</p>
+    <p>Hometown: East Grand Rapids, MI</p>
+    <p>Quote: "I first got into lifting weights during high school for basketball, and it developed into something I love to do to keep me active throughout the week. I love having a consistent schedule so that I can hold myself accountable. Lastly, I’ve met so many cool people from lifting at the gym, and I feel like everyone should give it a try!"</p>
     </div>
     
     
     <div className="trainer">
     <h3>Cole Ellis</h3>
     <p>Position: Lead Instructor</p>
-    <p>Hometown:</p>
-    <p>Fun Fact:</p>
+    <p>Hometown: Palm Beach, Florida</p>
+    <p>Quote: "I love lifting to push the limits of what my body can do, and overall living a healthier lifestyle."</p>
     </div>
     
     
     <div className="trainer">
     <h3>Sarah Buonnano</h3>
     <p>Position: Head Coordinator</p>
-    <p>Hometown:</p>
-    <p>Fun Fact:</p>
+    <p>Hometown: Boulder, Colorado</p>
+    <p>Quote: "After 16 years of dancing recreationally and competitively, i transitioned into the fitness world and developed a love for lifting, running, and pilates that has held strong for the past 3 years. I aspire to help others (especially women) build strength and confidence both physically and mentally."</p>
     </div>
     
     
     <div className="trainer">
     <h3>Alyssa Bersamin</h3>
     <p>Position: Social Media Chair</p>
-    <p>Hometown:</p>
-    <p>Fun Fact:</p>
+    <p>Hometown: Honolulu, Hawaii</p>
+    <p>Quote: "Strength does not come from winning. Your struggles develop your strengths. When you go through hardships and decide not to surrender, that is strength."</p>
     </div>
     
     
     <div className="trainer">
     <h3>Bita Tavafoghi</h3>
     <p>Position: Social Events Chair</p>
-    <p>Hometown:</p>
-    <p>Fun Fact:</p>
+    <p>Hometown: Nova</p>
+    <p>Fun Fact: "In my home town, the weights section was always dominated by men but one day I decided I wanted to try lifting, so I got my sister to try with me. I’ve been hooked ever since."</p>
     </div>
     </div>
     <Footer />
